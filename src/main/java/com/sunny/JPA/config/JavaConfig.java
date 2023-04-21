99e76e5be06d17810d40f1be45cc632e31fb1166
Sunny <godfks7636@naver.com>
2023-04-21 18:07:45 +0900

    Config파일 설정 주석 추가.

--- src/main/java/com/sunny/JPA/config/JavaConfig.java
+++ src/main/java/com/sunny/JPA/config/JavaConfig.java
@@ -18,13 +18,17 @@ import java.util.Properties;
 @Configuration
 @EnableJpaRepositories(basePackageClasses = RepositoryBase.class)
 public class JavaConfig {
+    // 순수 JPA 환경에서는 Persistence.EntityManagerFactory 사용해서 EntityManagerFactory 생성 가능.
+    // 스프링 환경에서는 LocalContainerEntityManagerFactoryBean을 사용해서 JPA 관리.
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(dataSource);
-        emf.setPackagesToScan("com.sunny.JPA.entity");
-        emf.setJpaVendorAdapter(jpaVendorAdapters());
-        emf.setJpaProperties(jpaProperties());
+        emf.setDataSource(dataSource); // 데이터베이스와의 연결을 설정하기 위한 javax.sql.DataSource을 설정.
+        emf.setPackagesToScan("com.sunny.JPA.entity"); // JPA Entity Class가 위치한 패키지를 지정.
+        emf.setJpaVendorAdapter(jpaVendorAdapters()); // JPA 구현체(vendor)에 대한 설정을 지정.
+        // vendor란 JPA는 표준 인터페이스이므로 여러가지 데이터베이스를 지원함. 그에 따라 데이터베이스마다 차이가 존재함.
+        // 이 차이점을 추상화하기 위해서 vendor라는 것이 존재.
+        emf.setJpaProperties(jpaProperties()); // JPA 구현체에 대한 추가 설정을 지정.
 
         return emf;
     }
